a80f8d3079b3fc5f78835cd03ef60e3b0c8a431c
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2021-12-08 01:38:25 +0530

    Minor Fixes

--- routes/predict.js
+++ routes/predict.js
@@ -2,8 +2,32 @@ const express = require('express');
 const router = express.Router();
 
 
-router.post('/upload', (req, res) => {
-   res.redirect('/result');    
+router.get('/', function(req, res) {
+   res.render('predict')    
+});
+
+router.post('/',function(req,res){
+   console.log(req.body)
+   
+   var data = [2349,18,1,0,0,0,0,0,1,1232,2,474,59,2107.05,2821.34,3213.44,4447.45,0.11,7.44,714.4,1094.09,2402.62,3260.58]
+   data = JSON.stringify(data);
+   var spawn = require('child_process').spawn;
+   var process = spawn('C:Users/prateek/anaconda3/envs/py37/python', ['E:/VIT/Sem 7/Tarp/Project/routes/predict.py', data]);
+
+   var predictedData;
+   process.stdout.on('data', function (data) {
+         predictedData = data.toString();
+   });
+
+   process.stdout.on('end', function (data) {
+         res.render("result",{predictedData:predictedData});
+      });
+
+
+   process.stderr.on('data', function (data) {
+      console.log(data.toString());
+      });
+
 });
 
 module.exports = router;
