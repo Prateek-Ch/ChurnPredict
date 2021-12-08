981a0c95b0efb198c69b1e95bb381d2a2dccdddd
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2021-12-08 16:40:40 +0530

    Fixed Weights

--- routes/predict.py
+++ routes/predict.py
@@ -11,38 +11,44 @@ sc = open('models/scaler.pkl','rb')
 scaler = pickle.load(sc)
 
 data = json.loads(sys.argv[1])
-data = np.array(list(data),dtype=float)
-data = data.reshape(1,-1)
+data = list(data)
+for i in range(9,20):
+    data[i] = np.log(data[i]+17000)
+    
+#%%
+data = np.array(data).reshape(1,-1)
 
-cols = ["gender","city","branch_code","days_since_last_transaction","occupation_company","occupation_retired","occupation_salaried","occupation_self_employed","occupation_student","customer_nw_category","current_balance","previous_month_end_balance","average_monthly_balance_prevQ2","average_monthly_balance_prevQ","current_month_credit","previous_month_credit","current_month_debit","previous_month_debit",	"current_month_balance","previous_month_balance","vintage","age","dependents"
-]
+#%%
+cols = ['gender', 'city', 'branch_code', 'days_since_last_transaction',
+        'occupation_company', 'occupation_retired', 'occupation_salaried',
+        'occupation_self_employed', 'occupation_student', 'customer_nw_category', 
+        'current_balance', 'previous_month_end_balance', 'average_monthly_balance_prevQ2',
+        'average_monthly_balance_prevQ', 'current_month_credit', 'previous_month_credit',
+        'current_month_debit', 'previous_month_debit', 'current_month_balance',
+        'previous_month_balance', 'vintage', 'age', 'dependents'
+        ]
 
-df = pd.DataFrame(data,columns=cols)
+df = pd.DataFrame(data,columns = cols)
 
-# print(json.dumps(df.columns.tolist()))
+#%%
+x = data[0][9:]
+x = np.array(x).reshape(1,-1)
 
-all_cols = ['customer_nw_category', 'current_balance',
-            'previous_month_end_balance', 'average_monthly_balance_prevQ2', 'average_monthly_balance_prevQ',
-            'current_month_credit','previous_month_credit', 'current_month_debit', 
-            'previous_month_debit','current_month_balance', 'previous_month_balance','vintage','age','dependents']
+#%%
+dataf = df.copy()
+#%%
+col_names = ['customer_nw_category', 
+        'current_balance', 'previous_month_end_balance', 'average_monthly_balance_prevQ2',
+        'average_monthly_balance_prevQ', 'current_month_credit', 'previous_month_credit',
+        'current_month_debit', 'previous_month_debit', 'current_month_balance',
+        'previous_month_balance', 'vintage', 'age', 'dependents']
+features = dataf[col_names]
+features = scaler.transform(features.values)
+#%%
+dataf[col_names] = features
 
-num_cols = ['customer_nw_category', 'current_balance',
-            'previous_month_end_balance', 'average_monthly_balance_prevQ2', 'average_monthly_balance_prevQ',
-            'current_month_credit','previous_month_credit', 'current_month_debit', 
-            'previous_month_debit','current_month_balance', 'previous_month_balance']
-
-for i in num_cols:
-    df[i] = np.log(df[i] + 17000)
-# print(json.dumps(df.values.tolist()))
-
-
-scaled = scaler.transform(df[all_cols])
-# print(json.dumps(scaled.tolist()))
-
-
-
-result = RFC_model.predict(scaled)
-
+#%%
+result = RFC_model.predict(dataf)
 print(json.dumps(result.item()))
 
 
