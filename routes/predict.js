3465e1111f290034faa1009edcb82fabc2cf4f64
Prateek Chaturvedi <prateekchaturvedi216@gmail.com>
2021-12-08 10:28:23 +0530

    Minor Fixes

--- routes/predict.js
+++ routes/predict.js
@@ -10,7 +10,7 @@ router.post('/',function(req,res){
    console.log(req.body)
    var a = req.body.vintage	
    var b = req.body.age	
-   var c = req.body.gender	
+   var c = req.body.Gender	
    var d = req.body.dependents	
    var e = req.body.occupation	
    var f = req.body.city	
@@ -27,8 +27,35 @@ router.post('/',function(req,res){
    var q = req.body.previous_month_debit	
    var r = req.body.current_month_balance	
    var s = req.body.previous_month_balance
+
+   console.log(c);
+   console.log(req.body.occupation)
+
+   if(c.localeCompare("Male")==0){
+      c = 1
+   }
+   else{
+      c = 0
+   }
    
-   var arr = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s]
+   var e1,e2,e3,e4;
+   if(e.localeCompare("retired")==0){
+      e1 = 1;
+      e2 = 0; e3 = 0;e4 = 0;
+   }
+   else if(e.localeCompare("salaried")==0){
+      e2 = 1;
+      e1=0;e3=0;e4 = 0;
+   }
+   else if(e.localeCompare("self-employed")==0){
+      e3 = 1;
+      e1=0;e2=0;e4 = 0;
+   }
+   else if(e.localeCompare("student")==0){
+      e4 = 1;
+      e1=0;e3=0;e2 = 0;
+   }
+   var arr = [a,b,c,d,0,e1,e2,e3,e4,f,g,h,i,j,k,l,m,n,o,p,q,r,s]
    console.log(arr)
 
    var data = [2349,18,1,0,0,0,0,0,1,1232,2,474,59,2107.05,2821.34,3213.44,4447.45,0.11,7.44,714.4,1094.09,2402.62,3260.58]
